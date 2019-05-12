fec43b71d7f99b4c2b41c428d90cf159493e5fe7
smallchung0724 <chung.ab@msa.hinet.net>
2019-05-12 23:24:16 +0800

    第一次簽入

---
+++ ASPdotNET_VSforMac_MVC_Cs_NetNote/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPdotNET_VSforMac_MVC_Cs_NetNote.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
